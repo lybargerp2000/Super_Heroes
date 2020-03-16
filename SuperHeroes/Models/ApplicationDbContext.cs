21fb53d3739c8e4d0eb3a24563114a515d35885c
Patrick Lybarger <lybargerp2000@yahoo.com>
2020-03-16 13:12:04 -0500

    Initial commit.

---
+++ SuperHeroes/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroes.Models
+{
+    public class ApplicationDbContext : IdentityDbContext 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(options)
+    {
+
+    }
+
+    {
+
+
+    }
+}
